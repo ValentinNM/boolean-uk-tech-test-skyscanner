2abc9107575e1fd2c8da0b5782230812306052db
ValentinNM <valentinm.web@gmail.com>
2022-01-13 17:35:11 +0200

    Extracted the value needed to replace

--- src/components/Flights/Flights.jsx
+++ src/components/Flights/Flights.jsx
@@ -4,8 +4,6 @@ import { useEffect, useState } from "react";
 import STYLES from "./Flights.scss";
 
 const Flights = () => {
-  const [itineraries, setItineraries] = useState([]);
-  const [legs, setLegs] = useState([]);
   const [flights, setFlights] = useState([]);
 
   const getFlights = () => {
@@ -17,11 +15,18 @@ const Flights = () => {
     })
       .then((res) => res.json())
       .then((data) => {
-        const iti = data.itineraries;
-        const leg = data.legs;
-        setItineraries(iti);
-        setLegs(leg);
-        setFlights(data.flights);
+
+        const {itineraries, legs} = data; 
+
+        // Step 1: map through the array we want to change
+        const foundFlights = data.itineraries.map((itinerary) => { 
+
+          // Step 2: map through each array value in order to
+          //        find our martching value in the target
+          const legsWithData = itinerary.legs.map((targetLegId) => 
+            legs.find((leg) => targetLegId === leg.id) // find and return the found data
+          )
+        })
       });
   };
   useEffect(() => {
@@ -31,47 +36,18 @@ const Flights = () => {
   return (
     <div>
       <ul>
-        {itineraries.map((itinerary, index) => {
-          const { price, agent, legs: ref } = itinerary;
-
-          const flights = legs.filter(
-            (leg) => leg.id === ref[0] || leg.id === ref[1]
-          );
-
-          let flight1 = flights.filter((flight) => flight.id === ref[0]);
-          let flight2 = flights.filter((flight) => flight.id === ref[1]);
-
-          return (
-            <li key={index}>
-              <div>
-                <div>
-                  {/* <img src={"https://logos.skyscnr.com/images/airlines/favicon/{flight1[0]airline_id}.png"} alt="airline_logo" /> */}
-                  {/* {flight1[0].departure_time} - {flight1[0].arrival_time}
-                  {flight1[0].departure_airport} - {flight1[0].arrival_airport}
-                  {flight1[0].duration_mins} mins
-                  {flight1[0].stops === 0
-                    ? "Direct"
-                    : flight1[0].stops + " Stop"} */}
-                </div>
-                <div>
-                  {/* {flight2[0].departure_time} - {flight2[0].arrival_time}
-                  {flight2[0].departure_airport} - {flight2[0].arrival_airport}
-                  {flight2[0].duration_mins} mins
-                  {flight2[0].stops === 0
-                    ? "Direct"
-                    : flight2[0].stops + " Stop"} */}
-                </div>
-              </div>
-              <div className="">
-                <div>
-                  <h3>{price}</h3>
-                  <p>{agent}</p>
-                </div>
-                <button>Select</button>
-              </div>
-            </li>
-          );
-        })}
+        <li>
+          <div>
+            <div>
+              {/* <img src={"https://logos.skyscnr.com/images/airlines/favicon/{flight1[0]airline_id}.png"} alt="airline_logo" /> */}
+            </div>
+            <div></div>
+          </div>
+          <div className="">
+            <div></div>
+            <button>Select</button>
+          </div>
+        </li>
       </ul>
     </div>
   );
